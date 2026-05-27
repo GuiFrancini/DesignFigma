20f38d3bf4f80cb63260799d9da425dd4078ecc5
Guilherme Francini <86390834+GuiFrancini@users.noreply.github.com>
2026-05-27 19:52:01 -0300

    ✨ feat:  new stylization

--- design-figma/src/components/header/header.tsx
+++ design-figma/src/components/header/header.tsx
@@ -2,12 +2,17 @@ export function Header() {
   return (
     <header className="w-full bg-white px-6 py-6">
       <div className="mx-auto flex max-w-7xl items-center justify-between">
-
-        <h1 className="text-2xl font-semibold tracking-tight text-black">
+ {/* div area  text-2xl font-normal tracking-tight text-black*/}
+        <h1 className="text-[30px] font-medium leading-[120%] tracking-[-0.05em] text-black">
+        
          <a href="#">Area </a>
         </h1>
 
-        <button className="flex items-center gap-1 rounded-full bg-[#485c11] px-5 py-2.5 text-sm font-medium text-white transition hover:bg-[#485c11]/90">
+ {/* div botão*/}
+        <button className="flex h-12 w-[127px] items-center justify-center
+         gap-[2px] rounded-full bg-[#485c11] px-[22px] py-[14px] text-center
+         text-sm font-bold leading-[140%] tracking-[-0.025em]
+         text-white transition hover:bg-[#8e8c78]/90">
           Learn More <span className="text-xs font-bold leading-nome">↗</span>
         </button>
       </div>
--- design-figma/src/components/navbar/navbar.tsx
+++ design-figma/src/components/navbar/navbar.tsx
@@ -1,22 +1,26 @@
 export function Navbar() {
   return (
     <div className="fixed left-1/2 top-6 z-50 -translate-x-1/2">
-      <nav className="flex items-center gap-6 rounded-full border
-         border-white/20 bg-white/10 px-6 py-4 backdrop-blur-xl">
+      <nav className="flex items-center gap-6 rounded-full 
+         bg-white/30 px-5 py-2.5 backdrop-blur-xl">
 
-        <a href="#" className="text-sm font-semibold text-black transition hover:text-[#929292]">
+        <a href="#" className="text-sm font-semibold
+         text-black transition-all duration-500 hover:text-[#929292]">
           Benefits
         </a>
 
-        <a href="#" className="text-sm font-semibold text-black transition hover:text-[#929292]">
+        <a href="#" className="text-sm font-semibold
+         text-black transition hover:text-[#929292]">
           Specifications
         </a>
 
-        <a href="#" className="text-sm font-semibold text-black transition hover:text-[#929292]">
+        <a href="#" className="text-sm font-semibold
+         text-black transition hover:text-[#929292]">
           How-to
         </a>
 
-        <a href="#" className="text-sm font-semibold text-black transition hover:text-[#929292]">
+        <a href="#" className="text-sm font-semibold
+         text-black transition hover:text-[#929292]">
           Contact Us
         </a>
       </nav>
