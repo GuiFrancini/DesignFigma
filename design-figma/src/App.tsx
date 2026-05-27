1bf70654d7704f0dfd32a44f499cfd86d039017b
Guilherme Francini <86390834+GuiFrancini@users.noreply.github.com>
2026-05-27 19:49:59 -0300

    ✨ feat:  add bigpicturesections and specs and benefits

--- design-figma/src/App.tsx
+++ design-figma/src/App.tsx
@@ -3,16 +3,20 @@ import { Navbar } from "./components/navbar/navbar";
 import { HeroSection } from "./sections/hero-section";
 import { LogoSection } from "./sections/logo-section";
 import { BenefitsSection } from "./sections/benefits-section";
+import {BigPictureSection} from "./sections/bigpicture-section";
+import { SpecsSection } from "./sections/specs-section";
 
 export function App() {
   return (
-    <div className="w-full min-h-screen bg-white text-zinc-900" >
+    <div className="w-full min-h-screen bg-white text-black" >
       <Header />
       <Navbar />
       <main>
         <HeroSection />
         <LogoSection />
         <BenefitsSection />
+        <BigPictureSection />
+        <SpecsSection />
       </main> 
       </div>
   );
