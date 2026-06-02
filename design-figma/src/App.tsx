8f186beae0d6811432e60e657925266df46c9033
Guilherme Francini <86390834+GuiFrancini@users.noreply.github.com>
2026-06-02 17:46:19 -0300

    ✨ feat: add new sections

--- design-figma/src/App.tsx
+++ design-figma/src/App.tsx
@@ -3,12 +3,16 @@ import { Navbar } from "./components/navbar/navbar";
 import { HeroSection } from "./sections/hero-section";
 import { LogoSection } from "./sections/logo-section";
 import { BenefitsSection } from "./sections/benefits-section";
-import {BigPictureSection} from "./sections/bigpicture-section";
+import { BigPictureSection } from "./sections/bigpicture-section";
 import { SpecsSection } from "./sections/specs-section";
+import { ImagemSection } from "./sections/imagem-section";
+import { HowToSection }  from "./sections/how-to-section";
+import { ContactUsSection}  from "./sections/contactus-section";
 
 export function App() {
+
   return (
-    <div className="w-full min-h-screen bg-white text-black" >
+    <div className="w-full min-h-screen bg-branco text-preto" >
       <Header />
       <Navbar />
       <main>
@@ -17,9 +21,13 @@ export function App() {
         <BenefitsSection />
         <BigPictureSection />
         <SpecsSection />
+        <ImagemSection />
+        <HowToSection />
+        <ContactUsSection />
       </main> 
       </div>
   );
 } 
 
+ 
 export default App;
